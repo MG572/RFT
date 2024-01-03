1152e2139302cb8768585d00b878b211aa83a17e
David0502-glitch <d.jozsa21@gmail.com>
2024-01-03 19:38:20 +0100

    Pontszám frissítés és elhelyezés a canvas-on

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ const previewY = 536;
 // Gravitáció
 const gravity = 0.1;
 const bounceStrength = 0.7;
+
+// Score update
+function drawScore() {
+    // Canvas méretének beállitása kép méret alapján
+    const dpr = window.devicePixelRatio || 1;
+    canvas1.width = 150 * dpr;
+    canvas1.height = 150 * dpr;
+
+    ctx1.scale(dpr, dpr);
+
+    ctx1.font = "20px Arial";
+    ctx1.fillStyle = "black";
+    ctx1.textAlign = "center";
+
+    ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+
+    ctx1.fillText("Score: " + score, canvas1.width / 2, canvas1.height / 6);
+}
