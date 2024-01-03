4fea9ca9bc63eb71cc98e327c96c9958159ec139
MG572 <martongergely2802@gmail.com>
2024-01-03 19:07:22 +0100

    Előre láthatólag szükséges változók bevezetése a JS-be

---
+++ script.js
@@ -0,0 +1,9 @@
+const canvas1 = document.getElementById('canvas1');
+const ctx1 = canvas1.getContext('2d');
+
+const canvas2 = document.getElementById('canvas2');
+const ctx2 = canvas2.getContext('2d');
+const balls = [];
+const colors = ['#ff0000', '#0000ff', '#00ff00', '#ffff00'];
+const sizes = [1, 2, 3, 4];
+let score = 0;
